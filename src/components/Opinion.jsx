0d69c165252516bbc39ada4763738a381bbdb20c
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-05 13:10:04 +0530

    feat:vote up and down functionality

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,4 +1,15 @@
+import { use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+  async function upvote() {
+    await upvoteOpinion(id);
+  }
+  async function downvote() {
+    await downvoteOpinion(id);
+  }
+
   return (
     <article>
       <header>
@@ -7,7 +18,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvote}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -27,7 +38,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvote}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
